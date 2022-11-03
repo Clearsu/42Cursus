3a2858b8a4f3b662ad9f29a96235dbf50d2f142c
Clearsu <clearwater1219@gmail.com>
2022-11-03 19:28:23 +0900

    mandatory argument error fixed

--- pipex/arg_check_bonus.c
+++ pipex/arg_check_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 15:01:42 by jincpark          #+#    #+#             */
-/*   Updated: 2022/11/01 15:42:26 by jincpark         ###   ########.fr       */
+/*   Updated: 2022/11/03 19:26:40 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	arg_check(int argc, char **argv)
 {
 	int	open_test;
 
-	if (ft_strncmp(argv[1], "here_doc", 8) == 0)
+	if (argv[1] && ft_strncmp(argv[1], "here_doc", 8) == 0)
 		return (1);
 	if (argc < 5)
 		ft_error(1, argv[argc - 1]);
