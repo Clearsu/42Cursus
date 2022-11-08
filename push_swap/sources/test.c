b57fe61709b8433bad3faa869a978755f4cce370
Clearsu <clearwater1219@gmail.com>
2022-11-08 17:03:31 +0900

    testing for operations

--- push_swap/sources/test.c
+++ push_swap/sources/test.c
@@ -3,16 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jincpark <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/08 16:28:21 by jincpark          #+#    #+#             */
-/*   Updated: 2022/11/08 16:28:43 by jincpark         ###   ########.fr       */
+/*   Created: 2022/11/08 16:46:31 by jincpark          #+#    #+#             */
+/*   Updated: 2022/11/08 16:46:54 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "push_swap.h"
 
 int	main(void)
 {
-	
+	t_stack	stack_a;
+	t_stack	stack_b;
+
+	make_stack(&stack_a, 5);
+	make_stack(&stack_b, 5);
+
+	for (int i = 0; i < 5; i++)
+		stack_a.stack[i] = i;
 }
