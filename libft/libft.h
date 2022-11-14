c70ae31837948faf14ee8ee2f6faaa34e8a95635
Clearsu <clearwater1219@gmail.com>
2022-11-14 23:19:25 +0900

    added ft_strcmp in libft

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jincpark <jincpark@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 20:24:04 by jincpark          #+#    #+#             */
-/*   Updated: 2022/11/04 18:27:53 by jincpark         ###   ########.fr       */
+/*   Updated: 2022/11/14 17:21:24 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strnstr(const char *big, const char *little, size_t blen);
 char	*ft_strdup(const char *s);
 int		ft_atoi(const char *nptr);
