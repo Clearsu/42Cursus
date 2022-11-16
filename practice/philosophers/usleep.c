89527d05919cf108a0f3e289b2db6b05cb5c5722
Clearsu <clearwater1219@gmail.com>
2022-11-16 23:02:58 +0900

    usleep practice

--- practice/philosophers/usleep.c
+++ practice/philosophers/usleep.c
@@ -6,10 +6,17 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 22:59:06 by jincpark          #+#    #+#             */
-/*   Updated: 2022/11/16 22:59:17 by jincpark         ###   ########.fr       */
+/*   Updated: 2022/11/16 23:02:21 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
 
+int main()
+{
+	printf("This thread is going to sleep...\n");
+	usleep(1000000);
+	printf("This thread has just waken up\n");
+}
 
