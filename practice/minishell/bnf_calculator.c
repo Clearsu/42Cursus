6b1d436058bc9f532daa341823d263e7dce4baae
Clearsu <clearwater1219@gmail.com>
2022-12-26 12:09:41 +0900

    making calculator

--- practice/minishell/bnf_calculator.c
+++ practice/minishell/bnf_calculator.c
@@ -6,24 +6,28 @@
 /*   By: jincpark <jincpark@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 19:13:13 by jincpark          #+#    #+#             */
-/*   Updated: 2022/12/23 19:22:49 by jincpark         ###   ########.fr       */
+/*   Updated: 2022/12/23 19:50:08 by jincpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* RULE */
 /*
 
-EXPRESSION 	::= EXPRESSION OPERATOR EXPRESSION
-			| PARENTHESIS EXPRESSION PARENTHESIS
-			| EXPRESSION OPERATOR NUMBER
-			| NUMBER
+EXPRESSION_1 	::= EXPRESSION_1 OPERATOR_1 EXPRESSION_2
+				| PARENTHESIS EXPRESSION_1 PARENTHESIS
+				| EXPRESSION_2
+
+EXPRESSION_2	::= EXPRESSION_2 OPERATOR_2 NUMBER
+				| NUMBER
+
+PARENTHESIS	::= ( | )
 
 NUMBER 		::= DIGIT NUMBER
 			| DIGIT
 
-OPERATOR	::= + | - | * | / | %
+OPERATOR_1	::= + | -
 
-PARENTHESIS	::= ( | )
+OPERATOR_2	::= * | / | %
 
 DIGIT 		::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
@@ -33,25 +37,41 @@ DIGIT 		::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 #include "ft_btree.h"
 #include "readline/readline.h"
 
-#define
+#define NUMBER 0
+#define OPERATOR_1 1
+#define OPERATOR_2 2
+#define PARENTHESIS 3
 
 struct typedef s_token
 {
-	int	type; // could be DIGIT or OPERATION
-	int	value;
+	int		type; // could be DIGIT or OPERATION
+	char	*value;
 }	t_token;
 
-void	parse(char **argv, t_list *head)
+void	parse(char *str, t_btree *btree)
 {
 	
+}
+
+int	compute(t_btree *btree)
+{
+
 }
 
 int	main(void)
 {	
 	char	*str;
+	t_btree	*btree;
+	int		res;
 
 	while (1)
 	{
-		str = 
+		str = readline("calculator >> ");
+		if (!ft_strcmp(str, "exit\n"))
+			break ;
+		parse(str, btree);
+		res = compute(btree);
+		ft_printf("result : %d\n", res);
 	}
+	return (0);
 }
